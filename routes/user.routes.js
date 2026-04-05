7e6f3bf67589273149022a8624e96991cbdeeeac
Prabash <mvsprabash@gmail.com>
2026-04-05 18:00:44 +0530

    refactor(users): modify user.router.js for better readability

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const { protect, authorize } = require('../middleware/auth.middleware.js');
+const { validate } = require('../middleware/validate.middleware.js');
+const { createUserSchema, updateUserSchema, idParamScheme } = require('../schemas/user.schema.js');
+
 const {
   getUsersController,
   createUserController,
@@ -11,28 +15,24 @@ const {
   updateMeController
 } = require('../controllers/user.controller.js');
 
-const { protect, authorize } = require('../middleware/auth.middleware.js');
 router.use(protect);
 
-// Get all users, admin only
+// Admin routes
 router.get('/', authorize("admin"), getUsersController);
-
-const { validate } = require('../middleware/validate.middleware.js');
-const { createUserSchema, updateUserSchema, idParamScheme } = require('../schemas/user.schema.js');
+router.post('/', authorize("admin"), validate(createUserSchema), createUserController);
+router.delete('/:id', authorize("admin"), validate(idParamScheme, "params"), deleteUserController);
 
 // Self Routes
 router.get('/me', getMeController);
 router.put('/me', validate(updateUserSchema), updateMeController);
 
-// CRUD operation for users
-router.post('/', authorize("admin"), validate(createUserSchema), createUserController);
+// id routes
 router.get('/:id', validate(idParamScheme, "params"), getUserByIdController);
 router.put('/:id', 
   validate(idParamScheme, "params"),
   validate(updateUserSchema),
   updateUserController
 );
-router.delete('/:id', authorize("admin"), validate(idParamScheme, "params"), deleteUserController);
 
 module.exports = router;
 
